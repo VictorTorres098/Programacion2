4068f348b8efbb9d392b1af9e228a8c37da6bb17
victor <victor@victor-H81M-H>
2021-09-02 22:19:28 -0300

    Sistema onDemand

---
+++ src/sistemaOneDemand/Sistema.java
@@ -0,0 +1,34 @@
+package sistemaOneDemand;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Sistema {
+	///para el catalogo de peliculas usaremos la estuctura de diccionario
+	HashMap<Integer, Pelicula> catalogoPeliculas;
+	
+	public Sistema() {
+		this.catalogoPeliculas = new HashMap<>();
+	}
+	public void agregarPelicula(Integer codigo,Pelicula pelicula) {
+		if(!exitePelicula(codigo)) {
+			catalogoPeliculas.put(codigo, pelicula);
+		}	
+	}
+	public boolean exitePelicula(Integer codigo) {
+		return catalogoPeliculas.containsKey(codigo);
+	}
+	public void bajarPelicula(Integer codigo) {
+		catalogoPeliculas.remove(codigo);
+	}
+	public void verPelicula(Integer codigo) {
+		catalogoPeliculas.get(codigo).addReproduccion();
+	}
+	public Integer consultarReproducciones(Integer codigo) {
+		return catalogoPeliculas.get(codigo).getReproducciones();
+	}
+	//Falta implementar mostrarPorGenero, mostarPorActor
+	
+	
+
+}
