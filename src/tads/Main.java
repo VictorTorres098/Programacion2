0a7d9301aaaf35100a575cf89106e40bd740a711
victortorres <victortorres098@gmail.com>
2021-09-07 14:53:48 -0300

    Se crearon conjuntos para tests

--- src/tads/Main.java
+++ src/tads/Main.java
@@ -10,6 +10,20 @@ public class Main {
 		elementos.agregar(233);
 		//System.out.println(elementos.MostrarConjunto());
 		//System.out.println(elementos.MostrarConjunto());
+		Conjunto elementosB = new Conjunto();
+		elementosB.agregar(454);
+		elementosB.agregar(54);
+		elementosB.agregar(14);
+		elementosB.agregar(124);
+		System.out.println(elementos.tamano());
+		elementos.union(elementosB);
+		System.out.println(elementos.tamano());
+		System.out.println(elementos.mostrarElementos());
+//		while(elementos!=null) {
+//			System.out.println(elementos.dameElemento());
+//			System.out.println(elementos.dameUnElemento());
+//		}
+
 
 	}
 
