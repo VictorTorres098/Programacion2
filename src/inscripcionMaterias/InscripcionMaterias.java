0e514618f5ff3000a5ef0c7591b8e5b2cf8ccd37
victortorres <victortorres098@gmail.com>
2021-09-13 15:53:57 -0300

    variables y constructor

--- src/inscripcionMaterias/InscripcionMaterias.java
+++ src/inscripcionMaterias/InscripcionMaterias.java
@@ -1,6 +1,16 @@
 package inscripcionMaterias;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class InscripcionMaterias {
+	private ArrayList<Alumno> alumnos;
+	//materias aun no decidi que estructura usar
+	private HashMap<Alumno,Materia> inscripcion;
 	
+	public InscripcionMaterias() {
+		alumnos = new ArrayList<Alumno>();
+		inscripcion = new HashMap<Alumno,Materia>();
+	}
 
 }
