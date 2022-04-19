89fcd323198d7c796f5ddb233da5cd03fe641743
victor <victortorres098@gmail.com>
2022-04-18 22:12:10 -0300

    # WARNING: head commit changed in the meantime
    test

--- src/tads/Main.java
+++ src/tads/Main.java
@@ -1,43 +1,20 @@
 package tads;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
 public class Main {
 
 	public static void main(String[] args) {
+		HashMap<Integer, String> map = new HashMap <> ();
+		map.put(1, "uno");
+		map.put(2, "dos");
 		
-		Conjunto elementos = new Conjunto();
-		elementos.agregar(1);
-		elementos.agregar(2);
-		elementos.agregar(3);
-		elementos.agregar(5);
-		elementos.agregar(6);
-		//System.out.println(elementos.MostrarConjunto());
-		//System.out.println(elementos.MostrarConjunto());
-		Conjunto elementosB = new Conjunto();
-		elementosB.agregar(7);
-		elementosB.agregar(3);
-		elementosB.agregar(15);
-		elementosB.agregar(6);
+		for(Integer claves : map.keySet()) {
+			System.out.println(map.get(claves));
+		}
 		
-//		System.out.println(elementos.tamano());
-//		System.out.println(elementosB.tamano());
-//		elementos.union(elementosB);
-//		System.out.println(elementos.tamano());
-//		System.out.println(elementosB.tamano());
-		
-		//deberia imprimir unicamente el item 3 ya que es el unico que se repite
-		elementos.interseccionB(elementosB);
-		
-//		for(int i = 0; i < elementos.tamano(); i++) {
-//			System.out.println(elementos.dameUnElemento());
-//		}
-		
-		/////////////////////////////////////////////DICCIONARIO//////////////////////////////////////////////////
-		Diccionario test = new Diccionario();
-		test.agregar(1, "uno");
-		test.agregar(2, "dos");
-//		System.out.println(test.buscarSignificado(1));
-		
-
 
 	}
 
